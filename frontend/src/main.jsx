ff3fa0b120595728ce0670583d125422c00e0060
pranav <pranav.pachpande09@gmail.com>
2024-12-08 16:36:35 -0500

    Clerk Auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/homePage/HomePage";
+import SignInPage from "./routes/signInPage/signInPage";
+import SignUpPage from "./routes/signUpPage/signUpPage";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+import DashboardPage from "./pages/dashboardPage/DashboardPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/homepage",
+        element: <HomePage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+        ],
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignInPage />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUpPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
