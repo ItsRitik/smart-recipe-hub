f97cc3db9b350fdb3ad4b33b9f0a1ce281febd73
ItsRitik <ri7ikkale27@gmail.com>
2024-12-09 20:05:05 -0500

    MonogoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -9,18 +10,52 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.error("Connection error:", err));
+// // MongoDB Connection with Stable API Version
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+// async function connectToDatabase() {
+//   try {
+//     // Connect to MongoDB using Mongoose and client options
+//     await mongoose.connect(process.env.MONGO_URI, clientOptions);
+//     console.log("MongoDB connected successfully!");
+//   } catch (err) {
+//     console.error("MongoDB connection failed", err);
+//   }
+// }
+
+// // Connect to MongoDB
+// connectToDatabase();
+const client = new MongoClient(process.env.MONGO_URI, {
+  
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-// Start Server
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
